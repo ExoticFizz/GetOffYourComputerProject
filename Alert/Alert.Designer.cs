2bc51774e607d54c08019e27340faa333d8c8572
ExoticFizz <lukemparker@outlook.com>
2015-01-07 16:55:47 +1000

    Revert "Revert "Test""
    This reverts commit 22da349f95192dd5bbc50381f6d41d6da1dec5bc.

---
+++ Alert/Alert.Designer.cs
@@ -0,0 +1,144 @@
+﻿using System.Diagnostics;
+namespace Alert
+    {
+    partial class Alert
+        {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+            {
+            if (disposing && (components != null))
+                {
+                components.Dispose();
+                }
+            base.Dispose(disposing);
+            }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+            {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Alert));
+            this.timMain = new System.Windows.Forms.Timer(this.components);
+            this.niTrayMain = new System.Windows.Forms.NotifyIcon(this.components);
+            this.lblGetOfIn = new System.Windows.Forms.Label();
+            this.lblGetOffTime = new System.Windows.Forms.Label();
+            this.lblGetOnTime = new System.Windows.Forms.Label();
+            this.lblGetBackOnIn = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // niTrayMain
+            // 
+            this.niTrayMain.Icon = ((System.Drawing.Icon)(resources.GetObject("niTrayMain.Icon")));
+            this.niTrayMain.Text = "Get Of Your Computer";
+            this.niTrayMain.Visible = true;
+            this.niTrayMain.MouseClick += new System.Windows.Forms.MouseEventHandler(this.niTrayMain_MouseClick);
+            // 
+            // lblGetOfIn
+            // 
+            this.lblGetOfIn.AutoSize = true;
+            this.lblGetOfIn.ForeColor = System.Drawing.Color.White;
+            this.lblGetOfIn.Location = new System.Drawing.Point(13, 13);
+            this.lblGetOfIn.Name = "lblGetOfIn";
+            this.lblGetOfIn.Size = new System.Drawing.Size(86, 13);
+            this.lblGetOfIn.TabIndex = 0;
+            this.lblGetOfIn.Text = "Time To Get Off:";
+            // 
+            // lblGetOffTime
+            // 
+            this.lblGetOffTime.AutoSize = true;
+            this.lblGetOffTime.ForeColor = System.Drawing.Color.White;
+            this.lblGetOffTime.Location = new System.Drawing.Point(106, 13);
+            this.lblGetOffTime.Name = "lblGetOffTime";
+            this.lblGetOffTime.Size = new System.Drawing.Size(52, 13);
+            this.lblGetOffTime.TabIndex = 1;
+            this.lblGetOffTime.Text = "Initializing";
+            // 
+            // lblGetOnTime
+            // 
+            this.lblGetOnTime.AutoSize = true;
+            this.lblGetOnTime.ForeColor = System.Drawing.Color.White;
+            this.lblGetOnTime.Location = new System.Drawing.Point(133, 41);
+            this.lblGetOnTime.Name = "lblGetOnTime";
+            this.lblGetOnTime.Size = new System.Drawing.Size(52, 13);
+            this.lblGetOnTime.TabIndex = 3;
+            this.lblGetOnTime.Text = "Initializing";
+            // 
+            // lblGetBackOnIn
+            // 
+            this.lblGetBackOnIn.AutoSize = true;
+            this.lblGetBackOnIn.ForeColor = System.Drawing.Color.White;
+            this.lblGetBackOnIn.Location = new System.Drawing.Point(13, 41);
+            this.lblGetBackOnIn.Name = "lblGetBackOnIn";
+            this.lblGetBackOnIn.Size = new System.Drawing.Size(114, 13);
+            this.lblGetBackOnIn.TabIndex = 2;
+            this.lblGetBackOnIn.Text = "Time To Get Back On:";
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(374, 63);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // Alert
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(374, 63);
+            this.Controls.Add(this.lblGetOnTime);
+            this.Controls.Add(this.lblGetBackOnIn);
+            this.Controls.Add(this.lblGetOffTime);
+            this.Controls.Add(this.lblGetOfIn);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Alert";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Alert";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Alert_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Alert_FormClosed);
+            this.Load += new System.EventHandler(this.Alert_Load);
+            this.Shown += new System.EventHandler(this.Alert_Shown);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Alert_MouseDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Alert_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Alert_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Alert_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timMain;
+        private System.Windows.Forms.NotifyIcon niTrayMain;
+        private System.Windows.Forms.Label lblGetOfIn;
+        private System.Windows.Forms.Label lblGetOffTime;
+        private System.Windows.Forms.Label lblGetOnTime;
+        private System.Windows.Forms.Label lblGetBackOnIn;
+        private System.Windows.Forms.Panel panel1;
+
+
+        }
+    }
+
---
+++ Alert/Alert.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using System.Xml;
+using System.IO;
+
+namespace Alert
+    {
+    public partial class Alert : Form
+        {
+        private Boolean jStart = new Boolean();
+        public Stopwatch s = new Stopwatch();
+        public Alert()
+            {
+            if (File.Exists(@"C:\Users\Public\file.txt"))
+                {
+                jStart = true;
+                // int.Parse(File.ReadAllText(@"C:\Users\Public\file.txt"));
+                string timeFrom = File.ReadAllText(@"C:\Users\Public\file.txt");
+
+                try
+                    {
+                    s.Elapsed.Add(TimeSpan.FromTicks(long.Parse(timeFrom)));
+                    }
+                catch (FormatException e)
+                    {
+                    MessageBox.Show("File Is Corrupt:\n" + e.Source + "\n" + e.StackTrace +"\n" + e.Message + "\n" + "Program will recreate the file.","",MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1,MessageBoxOptions.DefaultDesktopOnly);
+                    }
+
+                
+                }
+            else
+                {
+                MessageBox.Show("Hello");
+                }
+            InitializeComponent();
+
+            }
+
+        
+        public bool dragging = false;
+        public Point dragCursorPoint;
+        public Point dragFormPoint;
+
+        public void Alert_MouseDown(object sender, MouseEventArgs e)
+            {
+            dragging = true;
+            dragCursorPoint = Cursor.Position;
+            dragFormPoint = this.Location;
+            }
+
+        public void Alert_MouseMove(object sender, MouseEventArgs e)
+            {
+            if (dragging)
+                {
+                Point dif = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
+                this.Location = Point.Add(dragFormPoint, new Size(dif));
+                }
+            }
+
+        public void Alert_MouseUp(object sender, MouseEventArgs e)
+            {
+            dragging = false;
+            }
+
+
+        public void Alert_Load(object sender, EventArgs e)
+            {
+            
+            }
+
+        public void thread2()
+            {
+            while (true)
+                {
+                this.TopMost = true;
+                Thread.Sleep(10);
+                }
+            }
+
+        public void Alert_Shown(object sender, EventArgs e)
+            {
+            Thread t1 = new Thread(Thread1);
+            Thread t2 = new Thread(thread2);
+            this.TopMost = true;
+            t1.Start();
+            //t2.Start();
+            }
+     
+        public void Thread1()
+            {
+            
+            while (s.ElapsedMilliseconds <= 540000)
+                {
+                if (this.Visible)
+                    {
+
+                    }
+                jStart = false;
+                s.Start();
+                TimeSpan ts = s.Elapsed;
+                DirectoryInfo d = new DirectoryInfo(@"C:\Users\Public");
+                d.GetAccessControl();
+
+                File.WriteAllText(Path.Combine(d.FullName, "file.txt"), ts.Ticks.ToString());
+
+                SetText(String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10));
+                Thread.Sleep(1000); 
+                }
+            //Takes 1 millisecond to do s.Stop();
+            s.Stop();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        delegate void SetTextCallback(string text);
+
+        public void SetText(string text)
+            {
+            if (this.lblGetOffTime.InvokeRequired && jStart == false)
+                {
+
+                SetTextCallback d = new SetTextCallback(SetText);
+            this.Invoke(d, new object[] { text });
+                }
+            else
+                {
+                this.lblGetOffTime.Text = text;
+                }
+            }
+
+        public void Alert_FormClosed(object sender, FormClosedEventArgs e)
+            {
+            
+            }
+
+        public void Alert_FormClosing(object sender, FormClosingEventArgs e)
+            {
+            e.Cancel = true;
+            }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+            {
+
+            ControlPaint.DrawBorder(e.Graphics, this.panel1.ClientRectangle, Color.Black, ButtonBorderStyle.Solid);
+            }
+
+
+
+
+
+
+
+
+
+        public void panel1_MouseDown(object sender, MouseEventArgs e)
+            {
+            dragging = true;
+            dragCursorPoint = Cursor.Position;
+            dragFormPoint = this.Location;
+            }
+
+        public void panel1_MouseMove(object sender, MouseEventArgs e)
+            {
+            if (dragging)
+                {
+                Point dif = Point.Subtract(Cursor.Position, new Size(dragCursorPoint));
+                this.Location = Point.Add(dragFormPoint, new Size(dif));
+                }
+            }
+
+        public void panel1_MouseUp(object sender, MouseEventArgs e)
+            {
+            dragging = false;
+            }
+
+        private void niTrayMain_MouseClick(object sender, MouseEventArgs e)
+            {
+            if (this.Visible)
+                {
+                this.Hide();
+                }
+            else if(!this.Visible)
+                {
+                this.Show();
+                }
+            }
+
+        
+
+
+
+
+
+
+
+        }
+    }
